6248a5363c24fb182c63c27c9c3391d860f357bb
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-08-05 18:59:09 -0300

    [feat] Caso dados inválidos sejam preenchidos o programa encerrará com uma mensagem.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,29 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        final Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.print("Por favor, digite o número de sua conta: ");
-        final int numero = scan.nextInt();
+        try (final Scanner scan = new Scanner(System.in).useLocale(Locale.US);) {
+            System.out.print("Por favor, digite o número de sua conta: ");
+            final int numero = scan.nextInt();
 
-        System.out.print("Por favor, digite o número da Agência: ");
-        String agencia = scan.next();
+            System.out.print("Por favor, digite o número da Agência: ");
+            String agencia = scan.next();
 
-        System.out.print("Por favor, digite o seu nome: ");
-        String nomeCliente = scan.next();
+            System.out.print("Por favor, digite o seu nome: ");
+            String nomeCliente = scan.next();
 
-        System.out.print("Por favor, digite o seu saldo: ");
-        final double saldo = scan.nextDouble();
+            System.out.print("Por favor, digite o seu saldo: ");
+            final double saldo = scan.nextDouble();
 
-        String exemploConcatMetodo = "Olá ".concat(nomeCliente);
-        String exemploSinal = ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ",";
-        String mensagem = String.format("%s%s conta %d e seu saldo %.2f já está disponível para saque.",
-                exemploConcatMetodo, exemploSinal, numero, saldo);
+            String exemploConcatMetodo = "Olá ".concat(nomeCliente);
+            String exemploSinal = ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ",";
+            String mensagem = String.format("%s%s conta %d e seu saldo %.2f já está disponível para saque.",
+                    exemploConcatMetodo, exemploSinal, numero, saldo);
 
-        System.out.println(mensagem);
+            System.out.println(mensagem);
 
-        scan.close();
+        } catch (InputMismatchException e) {
+            System.out.println("Por favor, não insira caracteres em campos que apenas permitem numeros");
+        }
     }
 }
