2eecc8c155feaa2d9f619cb2e69e9cfdb687dfa8
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-08-05 18:58:01 -0300

    [feat] Agora é possivel criar uma conta no banco usando os dados fornecidos pelo terminal.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world! :D");
+        final Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Por favor, digite o número de sua conta: ");
+        final int numero = scan.nextInt();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scan.next();
+
+        System.out.print("Por favor, digite o seu nome: ");
+        String nomeCliente = scan.next();
+
+        System.out.print("Por favor, digite o seu saldo: ");
+        final double saldo = scan.nextDouble();
+
+        String exemploConcatMetodo = "Olá ".concat(nomeCliente);
+        String exemploSinal = ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ",";
+        String mensagem = String.format("%s%s conta %d e seu saldo %.2f já está disponível para saque.",
+                exemploConcatMetodo, exemploSinal, numero, saldo);
+
+        System.out.println(mensagem);
+
+        scan.close();
     }
 }
