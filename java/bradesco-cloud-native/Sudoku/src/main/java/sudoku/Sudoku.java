48da50345e29263443d67891cb97249f5cc5e786
Paulo Ricardo <pauloricardopaa@gmail.com>
2025-05-05 18:16:29 -0300

    [Sudoku](feat) Sudoku agora pode checar a validade do puzzle

--- java/bradesco-cloud-native/Sudoku/src/main/java/sudoku/Sudoku.java
+++ java/bradesco-cloud-native/Sudoku/src/main/java/sudoku/Sudoku.java
@@ -1,8 +1,13 @@
 package sudoku;
 
-import java.util.*;
-
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public class Sudoku {
 
@@ -11,9 +16,11 @@ public class Sudoku {
     public static final int BOARD_SIZE = BOARD_WIDTH * BOARD_HEIGHT;
 
     private final List<Cell> board;
+    private boolean lock = false;
 
     public Sudoku() {
         this.board = new ArrayList<>(BOARD_SIZE);
+        this.lock = false;
         for (int i = 0; i < BOARD_SIZE; i++) {
             this.board.add(i, new Cell(0));
         }
@@ -23,11 +30,12 @@ public class Sudoku {
         this();
         for (int i = 0; i < BOARD_SIZE; i++) {
             Integer number = board.get(i);
-            this.board.add(i, new Cell(number, number != 0));
+            this.board.set(i, new Cell(number, number != 0));
         }
     }
 
     public boolean set(int row, int col, int value) {
+        if (lock) return false;
         return get(row, col)
                 .map(cell -> cell.setNumber(value))
                 .orElse(false);
@@ -58,6 +66,62 @@ public class Sudoku {
         cell.unmark(positions);
     }
 
+    public boolean isColumnValid(int col) {
+        if (col < 0 || col >= BOARD_WIDTH) {
+            return false;
+        }
+        Set<Integer> values = new HashSet<>();
+        int i = -1;
+        while (++i < BOARD_WIDTH) {
+            Cell cell = get(indexForCoord(i, col));
+            if (isNull(cell))
+                return false;
+            if (cell.getNumber() == 0)
+                continue;
+            if (!values.add(cell.getNumber()))
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isLineValid(int row) {
+        if (row < 0 || row >= BOARD_HEIGHT) {
+            return false;
+        }
+        Set<Integer> values = new HashSet<>();
+        int i = -1;
+        while (++i < BOARD_HEIGHT) {
+            Cell cell = get(indexForCoord(row, i));
+            if (isNull(cell))
+                return false;
+            if (cell.getNumber() == 0)
+                continue;
+            if (!values.add(cell.getNumber()))
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isBlockValid(int row, int col) {
+        if (isOutBounds(indexForCoord(row, col)))
+            return false;
+        final int blockRow = ((int) row / 3) * 3;
+        final int blockCol = ((int) col / 3) * 3;
+        Set<Integer> values = new HashSet<>();
+        for (int i = blockRow; i < blockRow + 3; i++) {
+            for (int j = blockCol; j < blockCol + 3; j++) {
+                Cell cell = get(indexForCoord(i, j));
+                if (isNull(cell))
+                    return false;
+                if (cell.getNumber() == 0)
+                    continue;
+                if (!values.add(cell.getNumber()))
+                    return false;
+            }
+        }
+        return true;
+    }
+
     private Cell get(int index) {
         if (isOutBounds(index))
             return null;
@@ -74,6 +138,24 @@ public class Sudoku {
 
     public int totalSize() {
         return getBoard().size();
+    }
+
+    public void complete() {
+        if (isComplete())
+            this.lock = true;
+    }
+
+    public boolean isComplete() {
+        return isValid() && this.board.stream()
+                .allMatch(cell -> nonNull(cell) && cell.getNumber() != 0);
+    }
+
+    public boolean isValid() {
+        for (int i = 0; i < BOARD_HEIGHT; i++) {
+            if (!(isLineValid(i) && isColumnValid(i) && isBlockValid(i, i)))
+                return false;
+        }
+        return true;
     }
 
     public static int getBoardWidth() {
@@ -105,4 +187,5 @@ public class Sudoku {
         }
         return boardString.toString();
     }
+
 }
--- java/bradesco-cloud-native/Sudoku/src/test/java/sudoku/SudokuTest.java
+++ java/bradesco-cloud-native/Sudoku/src/test/java/sudoku/SudokuTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.InputStream;
 import java.util.Optional;
 import java.util.function.BiConsumer;
 
@@ -12,11 +13,21 @@ import org.junit.jupiter.api.Test;
 
 public class SudokuTest {
 
+    public static final String SUDOKU_PUZZLE_PATH = "/sudoku";
+    public static final String SUDOKU_INVALID_PUZZLE_PATH = "/sudoku/invalid";
+    public static final String SUDOKU_1 = "/1.txt";
+    private static final String SUDOKU_COL = "/col.txt";
+    private static final String SUDOKU_ROW = "/row.txt";
+    private static final String SUDOKU_BLOCK = "/block.txt";
+    private static final String SUDOKU_COMPLETE = "/complete.txt";
+
     private Sudoku sudoku;
+    private SudokuParser<InputStream> parser;
 
     @BeforeEach
     public void setup() {
         sudoku = new Sudoku();
+        parser = new TextMatrixSudokuParser();
     }
 
     @Test
@@ -84,6 +95,81 @@ public class SudokuTest {
             assertEquals(cell.get().getNumber(), (row % 10));
         };
         forEachCell(setForEachCell, assertGetForEachCell);
+    }
+
+    @Test
+    void testWhenCompleteShouldReturnTrue() {
+        assertTrue(parse(SUDOKU_COMPLETE).isComplete());
+    }
+
+    @Test
+    void testWhenCompleteShouldReturnFalse() {
+        String[] puzzles = { SUDOKU_COL, SUDOKU_ROW, SUDOKU_BLOCK };
+        for (int i = 0; i < puzzles.length; i++) {
+            assertFalse(parse(puzzles[i]).isComplete());
+            assertFalse(parseInvalid(puzzles[i]).isComplete());
+        }
+        assertFalse(parse(SUDOKU_1).isComplete());
+    }
+
+    @Test
+    void testWhenCheckColumnShouldReturnFalse() {
+        Sudoku col = parseInvalid(SUDOKU_COL);
+        assertFalse(col.isColumnValid(5));
+    }
+
+    @Test
+    void testWhenCheckLineShouldReturnFalse() {
+        Sudoku row = parseInvalid(SUDOKU_ROW);
+        assertFalse(row.isLineValid(1));
+    }
+
+    @Test
+    void testWhenCheckBlockShouldReturnFalse() {
+        Sudoku block = parseInvalid(SUDOKU_BLOCK);
+        assertFalse(block.isBlockValid(6, 3));
+        assertFalse(block.isBlockValid(7, 4));
+        assertFalse(block.isBlockValid(8, 5));
+    }
+
+    @Test
+    void testWhenCheckColumnShouldReturnTrue() {
+        sudoku = parse(SUDOKU_1);
+        Sudoku col = parse(SUDOKU_COL);
+        for (int i = 0; i < Sudoku.BOARD_WIDTH; i++) {
+            assertTrue(sudoku.isColumnValid(i));
+            assertTrue(col.isColumnValid(i));
+        }
+    }
+
+    @Test
+    void testWhenCheckLineShouldReturnTrue() {
+        sudoku = parse(SUDOKU_1);
+        Sudoku row = parse(SUDOKU_ROW);
+        for (int i = 0; i < Sudoku.BOARD_HEIGHT; i++) {
+            assertTrue(sudoku.isLineValid(i));
+            assertTrue(row.isLineValid(i));
+        }
+    }
+
+    @Test
+    void testWhenCheckBlockShouldReturnTrue() {
+        sudoku = parse(SUDOKU_1);
+        Sudoku block = parse(SUDOKU_BLOCK);
+        for (int i = 0; i < Sudoku.BOARD_HEIGHT; i++) {
+            for (int j = 0; j < Sudoku.BOARD_WIDTH; j++) {
+                assertTrue(sudoku.isBlockValid(i, j));
+                assertTrue(block.isBlockValid(i, j));
+            }
+        }
+    }
+
+    private Sudoku parseInvalid(String file) {
+        return parser.parse(SudokuTest.class.getResourceAsStream(SUDOKU_INVALID_PUZZLE_PATH + file));
+    }
+
+    private Sudoku parse(String file) {
+        return parser.parse(SudokuTest.class.getResourceAsStream(SUDOKU_PUZZLE_PATH + file));
     }
 
     private void forEachCell(BiConsumer<Integer, Integer> action,
